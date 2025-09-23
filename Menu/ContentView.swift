bdf311ac27b6cc3353b134da0ad73b0bca07d66e
littleguixiaogui <135602370+littleguixiaogui@users.noreply.github.com>
2025-09-23 01:49:36 +0100

    remove土豆丝 from the DishList because I can't cut it thin like that, remove useless sentences.

--- Menu/ContentView.swift
+++ Menu/ContentView.swift
@@ -33,9 +33,8 @@ struct ContentView: View {
     ]
     @State private var DishList:[DishItem]=[
         DishItem(name:"青菜"),
-        DishItem(name:"红烧肉second new branch test, changes in main branch"),
+        DishItem(name:"红烧肉"),
         DishItem(name:"蒜蓉炒菜"),
-        DishItem(name:"土豆丝changing just for github repo test")
     ]
     @State private var DessertList:[DessertItem]=[
         DessertItem(name:"empty")
