ed483251ee3bf154fb33673e41f2a41aa95535bb
littleguixiaogui <135602370+littleguixiaogui@users.noreply.github.com>
2025-09-27 00:16:44 +0100

    Version 2.0 - New Features: list of ingredients shows on separate list.

--- Menu/ContentView.swift
+++ Menu/ContentView.swift
@@ -12,39 +12,95 @@ struct FoodItem: Identifiable {
     let id = UUID()
     let name: String
     var isFoodChoose: Bool = false
+    let foodIngredient: [String]
 }
 
 struct DishItem: Identifiable {
     let id = UUID()
     let name: String
     var isDishChoose: Bool = false
+    let dishIngredient: [String]
 }
 
 struct DessertItem: Identifiable {
     let id = UUID()
     let name: String
     var isDessertChoose: Bool = false
+    let dessertIngredient: [String]
 }
+
+
+
+struct TopButton: View {
+    let topTitle: String
+    let buttonActive: Bool
+    let buttonAction: () -> Void
+    
+    var body: some View {
+        Text(topTitle)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+            .frame(width: 160, height: 70)
+            .background(RoundedRectangle(cornerRadius: 20).fill(Color.gray))
+            .onTapGesture {
+                buttonAction()
+            }
+    }
+}
+
+struct MenuButton: View {
+    let title: String
+    let isActive: Bool
+    let action: () -> Void
+    
+    //menu button
+    var body: some View{
+        Text(title)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+            .frame(width: 160, height: 70, alignment: .trailing)
+            .padding(.trailing)
+            .background(RoundedRectangle(cornerRadius: 20).fill(Color.gray))
+            .offset(x:isActive ? -50:-90)
+            .onTapGesture {
+                withAnimation {
+                    action()
+                }
+            }
+    }
+}
+
 
 struct ContentView: View {
     @State private var FoodList:[FoodItem]=[
-        FoodItem(name: "米饭"),
-        FoodItem(name: "面条")
+        FoodItem(name: "米饭",foodIngredient:["米饭"]),
+        FoodItem(name: "面条",foodIngredient:["面条"])
     ]
     @State private var DishList:[DishItem]=[
-        DishItem(name:"青菜"),
-        DishItem(name:"红烧肉"),
-        DishItem(name:"蒜蓉炒菜"),
+        DishItem(name:"青菜",dishIngredient: ["青菜"]),
+        DishItem(name:"红烧肉",dishIngredient: ["五花肉"]),
+        DishItem(name:"蒜蓉炒菜",dishIngredient: ["蒜蓉","青菜"]),
     ]
     @State private var DessertList:[DessertItem]=[
-        DessertItem(name:"提拉米苏"),
-        DessertItem(name: "姜撞奶")
+        DessertItem(name:"提拉米苏",dessertIngredient: ["提","拉","米","苏",]),
+        DessertItem(name: "姜撞奶",dessertIngredient: ["姜","奶"])
     ]
+    
+    
+    
+    
+    @State private var showMainMenu: Bool = true
+    @State private var showListMenu: Bool = false
+    
     
     @State private var showDishlist: Bool = false
     @State private var showFoodlist: Bool = false
     @State private var showDessertList:Bool = false
     @State private var showGoShop:Bool = true
+    
+   
     
     
     var body: some View {
@@ -56,141 +112,170 @@ struct ContentView: View {
             return "\(foods)\(dishes)\(desserts)"
         }
         
-        HStack{
-            VStack{
-                Text("主食")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                    .frame(width: 160, height: 70, alignment: .trailing)
-                    .padding(.trailing)
-                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.gray))
-                    .offset(x:showFoodlist ? -50:-90)
-                    .onTapGesture {
-                        withAnimation {
-                            showFoodlist=true
-                            showDishlist=false
-                            showDessertList=false
-                            showGoShop=false
-                        }
+        var ingredientList: [String] {
+            let foodIngredientList=FoodList.filter{$0.isFoodChoose}.flatMap{$0.foodIngredient}
+            let dishIngredientList=DishList.filter{$0.isDishChoose}.flatMap{$0.dishIngredient}
+            let dessertIngredientList=DessertList.filter{$0.isDessertChoose}.flatMap{$0.dessertIngredient}
+            return foodIngredientList+dishIngredientList+dessertIngredientList
+        }
+        
+        VStack{
+            Spacer()
+            HStack{
+                TopButton(topTitle:"主页",buttonActive:showMainMenu){
+                    withAnimation{
+                        showMainMenu=true
+                        showListMenu=false
                     }
-                Text("主菜")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                    .frame(width: 160, height: 70, alignment: .trailing)
-                    .padding(.trailing)
-                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.gray))
-                    .offset(x:showDishlist ? -50:-90)
-                    .onTapGesture {
-                        withAnimation {
-                            showFoodlist=false
-                            showDishlist=true
-                            showDessertList=false
-                            showGoShop=false
-                        }
+                }
+                
+                Spacer()
+                TopButton(topTitle:"列表",buttonActive:showListMenu){
+                    withAnimation{
+                        showMainMenu=false
+                        showListMenu=true
                     }
-                Text("甜点")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                    .frame(width: 160, height: 70, alignment: .trailing)
-                    .padding(.trailing)
-                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.gray))
-                    .offset(x:showDessertList ? -50:-90)
-                    .onTapGesture{
-                        withAnimation {
-                            showFoodlist=false
-                            showDishlist=false
-                            showDessertList=true
-                            showGoShop=false
-                        }
-                    }
-                Text("结算")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                    .frame(width: 160, height: 70, alignment: .trailing)
-                    .padding(.trailing)
-                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.gray))
-                    .offset(x:showGoShop ? -50:-90)
-                    .onTapGesture {
-                        withAnimation {
-                            showFoodlist=false
-                            showDishlist=false
-                            showDessertList=false
-                            showGoShop=true
-                        }
-                    }
+                }
+                
             }
+            .padding()
             Spacer()
             VStack{
-                if showFoodlist{
-                    VStack{
-                        ForEach($FoodList) { $food in
-                            HStack{
-                                Button{
-                                    food.isFoodChoose.toggle()
-                                }label:{
-                                    Image(systemName:food.isFoodChoose ? "heart.fill":"heart")
-                                        .foregroundStyle(.green)
-                                        .font(.system(size: 25))
-                                    Text(food.name)
-                                        .font(.system(size: 25))
-                                        .padding()
-                                }
-                            }
-                        }
-                    }
-                }
-                if showDishlist{
-                    VStack{
-                        ForEach($DishList) { $dish in
-                            HStack{
-                                Button{
-                                    dish.isDishChoose.toggle()
-                                    
-                                }label:{
-                                    Image(systemName:dish.isDishChoose ? "heart.fill":"heart")
-                                        .foregroundStyle(.green)
-                                        .font(.system(size: 25))
-                                    Text(dish.name)
-                                        .font(.system(size: 25))
-                                        .padding()
-                                }
-                            }
-                        }
-                    }
-                }
-                if showDessertList{
-                    VStack{
-                        ForEach($DessertList){ $dessert in
-                            HStack{
-                                Button{
-                                    dessert.isDessertChoose.toggle()
-                                }label:{
-                                    Image(systemName: dessert.isDessertChoose ? "heart.fill" : "heart")
-                                        .foregroundStyle(.green)
-                                        .font(.system(size: 25))
-                                    Text(dessert.name)
-                                        .font(.system(size: 25))
-                                        .padding()
-                                }
-                            }
-                        }
-                    }
-                }
-                if showGoShop{
+                //showMainMenu
+                if showMainMenu{
+                    Spacer()
                     HStack{
-                        Text(itemNumber)
-                            .font(.system(size: 25))
-                        Button ("结算"){
-                            
+                        VStack{
+                            MenuButton(title:"主食",isActive: showFoodlist){
+                                withAnimation {
+                                    showFoodlist=true
+                                    showDishlist=false
+                                    showDessertList=false
+                                    showGoShop=false
+                                }
+                            }
+                            MenuButton(title:"主菜",isActive: showDishlist){
+                                withAnimation {
+                                    showFoodlist=false
+                                    showDishlist=true
+                                    showDessertList=false
+                                    showGoShop=false
+                                }
+                            }
+                            MenuButton(title:"甜点",isActive: showDessertList){
+                                withAnimation {
+                                    showFoodlist=false
+                                    showDishlist=false
+                                    showDessertList=true
+                                    showGoShop=false
+                                }
+                            }
+                            MenuButton(title:"结算",isActive: showGoShop){
+                                withAnimation {
+                                    showFoodlist=false
+                                    showDishlist=false
+                                    showDessertList=false
+                                    showGoShop=true
+                                }
+                            }
+                        }
+                        Spacer()
+                        VStack{
+                            if showFoodlist{
+                                VStack{
+                                    ForEach($FoodList) { $food in
+                                        HStack{
+                                            Button{
+                                                food.isFoodChoose.toggle()
+                                            }label:{
+                                                Image(systemName:food.isFoodChoose ? "heart.fill":"heart")
+                                                    .foregroundStyle(.green)
+                                                    .font(.system(size: 25))
+                                                Text(food.name)
+                                                    .font(.system(size: 25))
+                                                    .padding()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if showDishlist{
+                                VStack{
+                                    ForEach($DishList) { $dish in
+                                        HStack{
+                                            Button{
+                                                dish.isDishChoose.toggle()
+                                            }label:{
+                                                Image(systemName:dish.isDishChoose ? "heart.fill":"heart")
+                                                    .foregroundStyle(.green)
+                                                    .font(.system(size: 25))
+                                                Text(dish.name)
+                                                    .font(.system(size: 25))
+                                                    .padding()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if showDessertList{
+                                VStack{
+                                    ForEach($DessertList){ $dessert in
+                                        HStack{
+                                            Button{
+                                                dessert.isDessertChoose.toggle()
+                                            }label:{
+                                                Image(systemName: dessert.isDessertChoose ? "heart.fill" : "heart")
+                                                    .foregroundStyle(.green)
+                                                    .font(.system(size: 25))
+                                                Text(dessert.name)
+                                                    .font(.system(size: 25))
+                                                    .padding()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if showGoShop{
+                                VStack{
+                                    Text(itemNumber)
+                                        .font(.system(size: 25))
+                                    Button ("结算"){
+                                    }
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                    Spacer()
+                }
+                
+                if showListMenu{
+ 
+                    ScrollView{
+                        HStack{
+                            VStack{
+                                if ingredientList.isEmpty{
+                                    Text("暂无食材")
+                                        .font(.system(size: 25))
+                                        .padding()
+                                }else{
+                                    Spacer()
+                                    ForEach(ingredientList, id: \.self) { ingredient in
+                                        HStack{
+                                            Text("icon")
+                                            Spacer()
+                                            Text(ingredient)
+                                        }
+                                    }
+                                    .font(.system(size: 25))
+                                    .padding()
+                                }
+                            }
                         }
                     }
                 }
+                Spacer()
             }
-            .padding(.trailing)
         }
     }
 }
